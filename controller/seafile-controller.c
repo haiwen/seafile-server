9fce30c9c544c928ae40433917ca0f4fe6da83bc
Jiaqiang Xu <jonathan.xu@seafile.com>
2016-08-27 12:20:39 +0800

    Fix compile error.

--- controller/seafile-controller.c
+++ controller/seafile-controller.c
@@ -320,6 +320,7 @@ setup_env ()
     g_setenv ("SEAFILE_CENTRAL_CONF_DIR", ctl->central_config_dir, TRUE);
 
     char *seahub_dir = g_build_filename (installpath, "seahub", NULL);
+    char *seafdav_conf = g_build_filename (ctl->central_config_dir, "seafdav.conf", NULL);
     g_setenv ("SEAHUB_DIR", seahub_dir, TRUE);
     g_setenv ("SEAFDAV_CONF", seafdav_conf, TRUE);
 
