46310d12a91fccdf47c3470a8c427871a73abe6b
Shuai Lin <linshuai2012@gmail.com>
2016-11-25 17:21:27 +0800

    No need to set ccnet peer name anymore.

--- scripts/setup-seafile-mysql.py
+++ scripts/setup-seafile-mysql.py
@@ -1258,10 +1258,7 @@ def get_param_val(arg, env, default=None):
     return arg or os.environ.get(env, default)
 
 def check_params(args):
-    host_name = socket.gethostname()
-    if len(host_name) > 15:
-        host_name = host_name[0:15]
-    server_name = get_param_val(args.server_name, 'SERVER_NAME', host_name)
+    server_name = 'seafile'
     ccnet_config.server_name = ccnet_config.validate_server_name(server_name)
 
     server_ip = get_param_val(args.server_ip, 'SERVER_IP',
