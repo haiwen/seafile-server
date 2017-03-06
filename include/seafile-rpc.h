16b71404a128004d10d639639782f8c6df841589
cuihaikuo <haikuo.cui@seafile.com>
2017-03-06 14:35:40 +0800

    RPC for role quota

--- include/seafile-rpc.h
+++ include/seafile-rpc.h
@@ -804,6 +804,9 @@ seafile_get_user_quota (const char *user, GError **error);
 int
 seafile_check_quota (const char *repo_id, gint64 delta, GError **error);
 
+int
+seafile_set_role_quota (const char *role, gint64 quota, GError **error);
+
 char *
 seafile_get_file_id_by_path (const char *repo_id, const char *path,
                              GError **error);
