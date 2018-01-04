c237df96809ca93e9706829ab5e1b017f6dfc582
Ultima1252 <ultima1252@gmail.com>
2018-01-04 14:27:21 -0800

    Update http-server.c

--- server/http-server.c
+++ server/http-server.c
@@ -2122,7 +2122,7 @@ http_server_run (void *arg)
 
     http_request_init (server);
 
-    evhtp_use_threads (priv->evhtp, NULL, server->worker_threads, NULL);
+    evhtp_use_threads_wexit (priv->evhtp, NULL, NULL, server->worker_threads, NULL);
 
     struct timeval tv;
     tv.tv_sec = CLEANING_INTERVAL_SEC;
