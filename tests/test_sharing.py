78277255989b05d74a7453071a696f585ab19744
Shuai Lin <linshuai2012@gmail.com>
2018-01-18 12:21:51 +0800

    tests: small refactor

--- tests/test_sharing.py
+++ tests/test_sharing.py
@@ -3,6 +3,7 @@ from seaserv import seafile_api as api
 from seaserv import ccnet_api
 
 from tests.config import ADMIN_USER, USER, USER2
+from tests.utils import assert_repo_with_permission
 
 
 @pytest.mark.parametrize('permission', ['r', 'rw'])
@@ -14,10 +15,7 @@ def test_share_repo_to_user(repo, permission):
     assert api.check_permission(repo.id, USER2) == permission
 
     repos = api.get_share_in_repo_list(USER2, 0, 1)
-    assert len(repos) == 1
-    r = repos[0]
-    assert r.id == repo.id
-    assert r.permission == permission
+    assert_repo_with_permission(repo, repos, permission)
 
     api.remove_share(repo.id, USER, USER2)
     assert api.check_permission(repo.id, USER2) is None
@@ -30,7 +28,7 @@ def test_share_repo_to_group(repo, group, permission):
 
     repos = api.get_repos_by_group(group.id)
     assert len(repos) == 0
-    
+
     group_list = ccnet_api.get_groups(USER)
     assert len(group_list) == 1
     group_list = ccnet_api.get_groups(USER2)
@@ -38,22 +36,16 @@ def test_share_repo_to_group(repo, group, permission):
 
     api.group_share_repo(repo.id, group.id, USER, permission)
     repos = api.get_repos_by_group(group.id)
-    assert len(repos) == 1
-    r = repos[0]
-    assert r.id == repo.id
-    assert r.permission == permission
+    assert_repo_with_permission(repo, repos, permission)
 
     ccnet_api.group_add_member(group.id, USER, USER2)
     group_list = ccnet_api.get_groups(USER2)
     assert len(group_list) == 1
-    g = group_list[0]
-    assert g.id == group.id
+    group = group_list[0]
+    assert group.id == group.id
 
-    repos2 = api.get_repos_by_group(g.id)
-    assert len(repos2) == 1
-    r2 = repos2[0]
-    assert r2.id == repo.id
-    assert r2.permission == permission
+    repos2 = api.get_repos_by_group(group.id)
+    assert_repo_with_permission(repo, repos2, permission)
 
     assert api.check_permission(repo.id, USER2) == permission
 
--- tests/utils.py
+++ tests/utils.py
@@ -18,3 +18,10 @@ def create_and_get_group(*a, **kw):
     group_id = ccnet_api.create_group(*a, **kw)
     group = ccnet_api.get_group(group_id)
     return group
+
+def assert_repo_with_permission(r1, r2, permission):
+    if isinstance(r2, list):
+        assert len(r2) == 1
+        r2 = r2[0]
+    assert r2.id == r1.id
+    assert r2.permission == permission
