65df9736612b27ad6cd6cedde15b07a364a25721
yixiang-zhang <35019657+yixiang-zhang@users.noreply.github.com>
2018-01-18 11:18:51 +0800

    share repo to group (#123)

--- tests/test_sharing.py
+++ tests/test_sharing.py
@@ -6,7 +6,8 @@ from tests.config import ADMIN_USER, USER, USER2
 
 
 @pytest.mark.parametrize('permission', ['r', 'rw'])
-def test_share_repo(repo, permission):
+def test_share_repo_to_user(repo, permission):
+    assert api.check_permission(repo.id, USER) == 'rw'
     assert api.check_permission(repo.id, USER2) is None
 
     api.share_repo(repo.id, USER, USER2, permission)
@@ -19,4 +20,45 @@ def test_share_repo(repo, permission):
     assert r.permission == permission
 
     api.remove_share(repo.id, USER, USER2)
+    assert api.check_permission(repo.id, USER2) is None
+
+
+@pytest.mark.parametrize('permission', ['r', 'rw'])
+def test_share_repo_to_group(repo, group, permission):
+    assert api.check_permission(repo.id, USER) == 'rw'
+    assert api.check_permission(repo.id, USER2) is None
+
+    repos = api.get_repos_by_group(group.id)
+    assert len(repos) == 0
+    
+    group_list = ccnet_api.get_groups(USER)
+    assert len(group_list) == 1
+    group_list = ccnet_api.get_groups(USER2)
+    assert len(group_list) == 0
+
+    api.group_share_repo(repo.id, group.id, USER, permission)
+    repos = api.get_repos_by_group(group.id)
+    assert len(repos) == 1
+    r = repos[0]
+    assert r.id == repo.id
+    assert r.permission == permission
+
+    ccnet_api.group_add_member(group.id, USER, USER2)
+    group_list = ccnet_api.get_groups(USER2)
+    assert len(group_list) == 1
+    g = group_list[0]
+    assert g.id == group.id
+
+    repos2 = api.get_repos_by_group(g.id)
+    assert len(repos2) == 1
+    r2 = repos2[0]
+    assert r2.id == repo.id
+    assert r2.permission == permission
+
+    assert api.check_permission(repo.id, USER2) == permission
+
+    api.group_unshare_repo(repo.id, group.id, USER);
+    repos = api.get_repos_by_group(group.id)
+    assert len(repos) == 0
+
     assert api.check_permission(repo.id, USER2) is None
