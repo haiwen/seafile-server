5c355e364dbb9c915a2bb05c190f2d78594d4de4
cuihaikuo <haikuo.cui@seafile.com>
2018-05-18 14:14:58 +0800

    Update seahub.sql.

--- scripts/upgrade/sql/6.3.0/mysql/seahub.sql
+++ scripts/upgrade/sql/6.3.0/mysql/seahub.sql
@@ -1,3 +1,14 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE IF NOT EXISTS `auth_group` (
@@ -92,6 +103,10 @@ CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
+/*!40000 ALTER TABLE `wiki_personalwiki` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wiki_personalwiki` ENABLE KEYS */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE IF NOT EXISTS `wiki_wiki` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
@@ -105,6 +120,20 @@ CREATE TABLE IF NOT EXISTS `wiki_wiki` (
   UNIQUE KEY `wiki_wiki_username_3c0f83e1b93de663_uniq` (`username`,`repo_id`),
   KEY `wiki_wiki_fde81f11` (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40000 ALTER TABLE `wiki_wiki` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wiki_wiki` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
 
 ALTER TABLE notifications_notification ADD INDEX `notifications_notification_386bba5a` (`primary`);
 
