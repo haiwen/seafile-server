72fe30c4e6756c110c8419cab1f6076e0c64ab51
ilearnit <helloworld.c@outlook.com>
2019-04-02 05:38:50 +0000

    update sql

---
+++ scripts/upgrade/sql/7.0.0/sqlite3/seafile.sql
@@ -0,0 +1,5 @@
+ALTER TABLE RepoInfo ADD COLUMN status INTEGER DEFAULT 0;
+ALTER TABLE RepoSyncError RENAME TO TmpRepoSyncError;
+CREATE TABLE RepoSyncError (token CHAR(41) PRIMARY KEY, error_time BIGINT, error_con VARCHAR(1024));
+INSERT INTO RepoSyncError SELECT * FROM TmpRepoSyncError;
+DROP TABLE TmpRepoSyncError; 
