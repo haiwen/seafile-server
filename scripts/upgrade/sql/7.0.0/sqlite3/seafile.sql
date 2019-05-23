182fdb20287867447569ce104f4bb9d3410605c1
Jonathan Xu <jonathan.xu@seafile.com>
2019-05-23 14:53:16 +0800

    Fix upgrade sql: create RepoSyncError table if not exists.

--- scripts/upgrade/sql/7.0.0/sqlite3/seafile.sql
+++ scripts/upgrade/sql/7.0.0/sqlite3/seafile.sql
@@ -1,4 +1,5 @@
 ALTER TABLE RepoInfo ADD COLUMN status INTEGER DEFAULT 0;
+CREATE TABLE IF NOT EXISTS RepoSyncError (token CHAR(41) PRIMARY KEY, error_time BIGINT, error_con VARCHAR(1024));
 ALTER TABLE RepoSyncError RENAME TO TmpRepoSyncError;
 CREATE TABLE RepoSyncError (token CHAR(41) PRIMARY KEY, error_time BIGINT, error_con VARCHAR(1024));
 INSERT INTO RepoSyncError SELECT * FROM TmpRepoSyncError;
