d7160507910e48db33911a96748cd31a78b1b10b
sniper-py <dzmbbs@qq.com>
2019-12-16 17:32:53 +0800

    fix

--- scripts/upgrade/sql/7.1.0/sqlite3/seahub.sql
+++ scripts/upgrade/sql/7.1.0/sqlite3/seahub.sql
@@ -10,12 +10,13 @@ DROP TABLE "constance_config_old";
 
 ALTER TABLE "drafts_draft" RENAME TO "drafts_draft_old";
 CREATE TABLE "drafts_draft" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "username" varchar(255) NOT NULL, "origin_file_version" varchar(100) NOT NULL, "draft_file_path" varchar(1024) NOT NULL, "origin_file_uuid" char(32) NOT NULL UNIQUE, "publish_file_version" varchar(100) NULL, "status" varchar(20) NOT NULL, "origin_repo_id" varchar(36) NOT NULL);
+INSERT INTO "drafts_draft" ("id", "created_at", "updated_at", "username", "origin_file_version", "draft_file_path", "origin_file_uuid", "publish_file_version", "status", "origin_repo_id") SELECT "id", "created_at", "updated_at", "username", "origin_file_version", "draft_file_path", "origin_file_uuid", "publish_file_version", "status", "origin_repo_id" FROM "drafts_draft_old";
+DROP TABLE "drafts_draft_old";
+
 CREATE INDEX "drafts_draft_created_at_e9f4523f" ON "drafts_draft" ("created_at");
 CREATE INDEX "drafts_draft_origin_repo_id_8978ca2c" ON "drafts_draft" ("origin_repo_id");
 CREATE INDEX "drafts_draft_updated_at_0a144b05" ON "drafts_draft" ("updated_at");
 CREATE INDEX "drafts_draft_username_73e6738b" ON "drafts_draft" ("username");
-INSERT INTO "drafts_draft" ("id", "created_at", "updated_at", "username", "origin_file_version", "draft_file_path", "origin_file_uuid", "publish_file_version", "status", "origin_repo_id") SELECT "id", "created_at", "updated_at", "username", "origin_file_version", "draft_file_path", "origin_file_uuid", "publish_file_version", "status", "origin_repo_id" FROM "drafts_draft_old";
-DROP TABLE "drafts_draft_old";
 
 
 CREATE TABLE "abuse_reports_abusereport" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "reporter" text NULL, "repo_id" varchar(36) NOT NULL, "repo_name" varchar(255) NOT NULL, "file_path" text NULL, "abuse_type" varchar(255) NOT NULL, "description" text NULL, "handled" bool NOT NULL, "time" datetime NOT NULL);
