ae9579e210cc91c653b75a8a26d7701d4146aeb3
feiniks <36756310+feiniks@users.noreply.github.com>
2020-05-20 18:36:38 +0800

    Regularly cleanup files in the httptemp directory (#343)
    * Regularly cleanup files in the httptemp directory
    
    * Add scan http temp dir default configuration
    
    * Fix mem leak for scan httptemp dir
    
    * Modify int to gint64

--- server/fileserver-config.h
+++ server/fileserver-config.h
@@ -9,6 +9,9 @@ fileserver_config_get_integer(GKeyFile *config, char *key, GError **error);
 char *
 fileserver_config_get_string(GKeyFile *config, char *key, GError **error);
 
+int
+fileserver_config_get_int64(GKeyFile *config, char *key, GError **error);
+
 gboolean
 fileserver_config_get_boolean(GKeyFile *config, char *key, GError **error);
 
