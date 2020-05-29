9d5369e70d69b785c5f7417cef1ad5dd202bcdeb
杨赫然 <heran.yang@seafile.com>
2020-05-29 18:00:49 +0800

    Add commitmgr and test case

--- fileserver/commitmgr/commitmgr.go
+++ fileserver/commitmgr/commitmgr.go
@@ -3,5 +3,75 @@ package commitmgr
 
 import (
 	// Change to non-blank import when use
-	_ "github.com/haiwen/seafile-server/fileserver/objstore"
+	"encoding/json"
+	"github.com/haiwen/seafile-server/fileserver/objstore"
+	"io"
 )
+
+type Commit struct {
+	CommitID       string  `json:"commit_id"`
+	RepoID         string  `json:"repo_id"`
+	RootID         string  `json:"root_id"`
+	CreaterName    string  `json:"creater_name,omitempty"`
+	CreaterID      string  `json:"creater"`
+	Desc           string  `json:"description"`
+	Ctime          int64   `json:"ctime"`
+	ParentID       *string `json:"parent_id"`
+	SecondParentID *string `json:"second_parent_id"`
+	RepoName       string  `json:"repo_name"`
+	RepoDesc       string  `json:"repo_desc"`
+	RepoCategory   *string `json:"repo_category"`
+	DeviceName     string  `json:"device_name,omitempty"`
+	ClientVersion  string  `json:"client_version,omitempty"`
+	Encrypted      string  `json:"encrypted,omitempty"`
+	EncVersion     int     `json:"enc_version,omitempty"`
+	Magic          string  `json:"magic,omitempty"`
+	RandomKey      string  `json:"key,omitempty"`
+	Salt           string  `json:"salt,omitempty"`
+	NoLocalHistory int     `json:"no_local_history,omitempty"`
+	Version        int     `json:"version,omitempty"`
+	Conflict       int     `json:"conflict,omitempty"`
+	NewMerge       int     `json:"new_merge,omitempty"`
+	Repaired       int     `json:"repaired,omitempty"`
+}
+
+//Write parse the JSON-encoded data and stores the result int the commit.
+func (commit *Commit) Write(p []byte) (int, error) {
+	err := json.Unmarshal(p, commit)
+	if err != nil {
+		return -1, err
+	}
+
+	return len(p), nil
+}
+
+//Read traverses the commit to JSON-encoded data.
+func (c *Commit) Read(p []byte) (int, error) {
+	jsonstr, err := json.Marshal(c)
+	if err != nil {
+		return -1, err
+	}
+	copy(p, jsonstr)
+
+	return len(jsonstr), io.EOF
+}
+
+//Load commit from storage backend.
+func (commit *Commit) LoadCommit(store *objstore.ObjectStore, repoID string, commitID string) error {
+	err := store.Read(repoID, commitID, commit)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//Save commit to storage backend.
+func (commit *Commit) SaveCommit(store *objstore.ObjectStore, repoID string, commitID string) error {
+	err := store.Write(repoID, commitID, commit, false)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ fileserver/commitmgr/commitmgr_test.go
@@ -0,0 +1,64 @@
+package commitmgr
+
+import (
+	"fmt"
+	"github.com/haiwen/seafile-server/fileserver/objstore"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	commitID        = "0401fc662e3bc87a41f299a907c056aaf8322a27"
+	repoID          = "b1f2ad61-9164-418a-a47f-ab805dbd5694"
+	seafileConfPath = "/tmp/conf"
+	seafileDataDir  = "/tmp/conf/seafile-data"
+)
+
+func delFile() error {
+	err := os.RemoveAll(seafileConfPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	err := delFile()
+	if err != nil {
+		fmt.Printf("Failed to remove test file : %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(code)
+}
+
+func assertEqual(t *testing.T, a, b interface{}) {
+	if a != b {
+		t.Errorf("Not Equal.%t,%t", a, b)
+	}
+}
+
+func TestCommit(t *testing.T) {
+	store := objstore.New(seafileConfPath, seafileDataDir, "commit")
+	newCommit := new(Commit)
+	newCommit.CommitID = commitID
+	newCommit.RepoID = repoID
+	newCommit.CreaterName = "seafile"
+	newCommit.CreaterID = commitID
+	newCommit.Desc = "This is a commit"
+	newCommit.Ctime = time.Now().Unix()
+	parentID := repoID
+	newCommit.ParentID = &parentID
+	newCommit.DeviceName = "Linux"
+	newCommit.SaveCommit(store, repoID, commitID)
+
+	commit := new(Commit)
+	commit.LoadCommit(store, repoID, commitID)
+	assertEqual(t, commit.CommitID, commitID)
+	assertEqual(t, commit.RepoID, repoID)
+	assertEqual(t, commit.CreaterName, "seafile")
+	assertEqual(t, commit.CreaterID, commitID)
+	assertEqual(t, *commit.ParentID, repoID)
+}
