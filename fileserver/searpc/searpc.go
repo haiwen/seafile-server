a85a552462099d90716552f8d7ee513f240af019
杨赫然 <heran.yang@seafile.com>
2020-06-09 18:23:32 +0800

    Add searpc package

--- fileserver/searpc/searpc.go
+++ fileserver/searpc/searpc.go
@@ -1,12 +1,32 @@
 // Package searpc implements searpc client protocol with unix pipe transport.
 package searpc
 
+import (
+	"bufio"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+)
+
 // Client represents a connections to the RPC server.
 type Client struct {
 	// path of the named pipe
 	pipePath string
 	// RPC service name
-	service string
+	Service string `json:"service"`
+	// RPC request
+	Request string `json:"request"`
+}
+
+// Init initializes rpc client.
+func Init(pipePath string, service string) *Client {
+	client := new(Client)
+	client.pipePath = pipePath
+	client.Service = service
+
+	return client
 }
 
 // Call calls the RPC function funcname with variadic parameters.
@@ -14,5 +34,56 @@ type Client struct {
 // The true returned type can be int32, int64, string, struct (object), list of struct (objects) or JSON
 func (c *Client) Call(funcname string, params ...interface{}) interface{} {
 	// TODO: use reflection to compose requests and parse results.
-	return nil
+	var unixAddr *net.UnixAddr
+	unixAddr, err := net.ResolveUnixAddr("unix", c.pipePath)
+	if err != nil {
+		fmt.Printf("failed to resolve unix addr : %v.\n", err)
+		return nil
+	}
+
+	conn, err := net.DialUnix("unix", nil, unixAddr)
+	if err != nil {
+		fmt.Printf("failed to dial unix : %v.\n", err)
+		return nil
+	}
+	defer conn.Close()
+
+	var request []interface{}
+	request = append(request, funcname)
+	request = append(request, params...)
+	jsonstr, err := json.Marshal(request)
+	if err != nil {
+		fmt.Printf("failed to convert object to json : %v\n", err)
+	}
+	c.Request = string(jsonstr)
+
+	jsonstr, err = json.Marshal(c)
+	if err != nil {
+		fmt.Printf("failed to convert object to json.\n")
+		return nil
+	}
+
+	header := make([]byte, 4)
+	binary.LittleEndian.PutUint32(header, uint32(len(jsonstr)))
+	conn.Write([]byte(header))
+
+	conn.Write([]byte(jsonstr))
+
+	reader := bufio.NewReader(conn)
+	buflen := make([]byte, 4)
+	_, err = io.ReadFull(reader, buflen)
+	if err != nil {
+		fmt.Printf("failed to read from rpc server : %v.\n", err)
+		return nil
+	}
+	retlen := binary.LittleEndian.Uint32(buflen)
+
+	msg := make([]byte, retlen)
+	_, err = io.ReadFull(reader, msg)
+	if err != nil {
+		fmt.Printf("failed to read from rpc server : %v.\n", err)
+		return nil
+	}
+
+	return msg
 }
