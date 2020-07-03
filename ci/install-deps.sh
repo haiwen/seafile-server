aba78c15c496ab54e5a71fb2cbe91b8aa6753eba
demian80 <demian.ionov@gmail.com>
2020-07-03 16:17:48 +0300

    Update install-deps.sh

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -10,7 +10,7 @@ cd $SETUP_DIR
 
 sudo apt-get update
 sudo apt-get install -y intltool libarchive-dev libcurl4-openssl-dev libevent-dev \
-libfuse-dev libglib2.0-dev libjansson-dev libmysqlclient-dev libonig-dev \
+libfuse-dev libglib2.0-dev libjansson-dev libmysqlclient-dev libpq-dev libonig-dev \
 sqlite3 libsqlite3-dev libtool net-tools uuid-dev valac mysql-client
 sudo service mysql start
 
