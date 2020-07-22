321f080f26868c2e821a506504dc16a1e43e93a7
Xiangyue Cai <caixiangyue007@gmail.com>
2020-07-22 16:03:50 +0800

    add /<repo-id>/commit/HEAD (#377)

--- fileserver/sync_api.go
+++ fileserver/sync_api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html"
 	"log"
@@ -139,6 +140,62 @@ func permissionCheckCB(rsp http.ResponseWriter, r *http.Request) *appError {
 			}
 		}
 	}
+	return nil
+}
+
+func headCommitOperCB(rsp http.ResponseWriter, r *http.Request) *appError {
+	if r.Method == http.MethodGet {
+		return getHeadCommit(rsp, r)
+	} else if r.Method == http.MethodPut {
+		// TODO: handle put
+		return nil
+	}
+	return &appError{nil, "", http.StatusBadRequest}
+}
+
+func getHeadCommit(rsp http.ResponseWriter, r *http.Request) *appError {
+	vars := mux.Vars(r)
+	repoID := vars["repoid"]
+	sqlStr := "SELECT EXISTS(SELECT 1 FROM Repo WHERE repo_id=?)"
+	var exists bool
+	row := seafileDB.QueryRow(sqlStr, repoID)
+	if err := row.Scan(&exists); err != nil {
+		if err != sql.ErrNoRows {
+			log.Printf("DB error when check repo %s existence: %v", repoID, err)
+			msg := `{"is_corrupted": 1}`
+			rsp.WriteHeader(http.StatusOK)
+			rsp.Write([]byte(msg))
+			return nil
+		}
+	}
+	if !exists {
+		return &appError{nil, "", seafHTTPResRepoDeleted}
+	}
+
+	if _, err := validateToken(r, repoID, false); err != nil {
+		return err
+	}
+
+	var commitID string
+	sqlStr = "SELECT commit_id FROM Branch WHERE name='master' AND repo_id=?"
+	row = seafileDB.QueryRow(sqlStr, repoID)
+
+	if err := row.Scan(&commitID); err != nil {
+		if err != sql.ErrNoRows {
+			log.Printf("DB error when get branch master: %v", err)
+			msg := `{"is_corrupted": 1}`
+			rsp.WriteHeader(http.StatusOK)
+			rsp.Write([]byte(msg))
+			return nil
+		}
+	}
+	if commitID == "" {
+		return &appError{nil, "", http.StatusBadRequest}
+	}
+
+	msg := fmt.Sprintf("{\"is_corrupted\": 0, \"head_commit_id\": \"%s\"}", commitID)
+	rsp.WriteHeader(http.StatusOK)
+	rsp.Write([]byte(msg))
 	return nil
 }
 
