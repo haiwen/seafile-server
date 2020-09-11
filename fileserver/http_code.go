d97db8d9fc97c0c95a06f14c56c383c618cc5ecb
feiniks <36756310+feiniks@users.noreply.github.com>
2020-09-11 10:49:21 +0800

    Add file update API (#399)
    * Add file update API
    
    * Add GetObjIDByPath and fix change size error
    
    * Add traffic statistics for update api

--- fileserver/http_code.go
+++ fileserver/http_code.go
@@ -1,8 +1,12 @@
 package main
 
 const (
+	seafHTTPResBadFileName   = 440
+	seafHTTPResExists        = 441
+	seafHTTPResNotExists     = 441
 	seafHTTPResTooLarge      = 442
 	seafHTTPResNoQuota       = 443
 	seafHTTPResRepoDeleted   = 444
 	seafHTTPResRepoCorrupted = 445
+	seafHTTPResBlockMissing  = 446
 )
