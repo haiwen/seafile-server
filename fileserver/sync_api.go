f2842df24febaf4b1dd10232532f3ba20ae4df35
杨赫然 <heran.yang@seafile.com>
2021-12-01 14:21:08 +0800

    Validate repo_id when validate token from cache for go

--- fileserver/sync_api.go
+++ fileserver/sync_api.go
@@ -1034,7 +1034,7 @@ func validateToken(r *http.Request, repoID string, skipCache bool) (string, *app
 	}
 
 	if value, ok := tokenCache.Load(token); ok {
-		if info, ok := value.(*tokenInfo); ok {
+		if info, ok := value.(*tokenInfo); ok && info.repoID == repoID {
 			return info.email, nil
 		}
 	}
