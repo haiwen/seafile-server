096828271ffd2ee40c75225ef347dfddb707eb93
杨赫然 <heran.yang@seafile.com>
2023-07-31 11:03:02 +0800

    Modify comment

--- server/gc/fsck.c
+++ server/gc/fsck.c
@@ -262,7 +262,7 @@ fsck_check_dir_recursive (const char *id, const char *parent_dir, FsckData *fsck
             if (seaf_dir_save (mgr, store_id, version, new_dir) < 0) {
                 seaf_warning ("Repo[%.8s] failed to save dir\n", fsck_data->repo->id);
                 seaf_dir_free (new_dir);
-                // dir->entries has beed freed.
+                // dir->entries was taken by new_dir, which has been freed.
                 dir->entries = NULL;
                 goto out;
             }
