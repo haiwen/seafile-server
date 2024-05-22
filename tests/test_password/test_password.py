368868f888a8c61f4dd9be664cbc46da5d188718
杨赫然 <heran.yang@seafile.com>
2024-05-22 17:09:57 +0800

    Fix ci error

--- tests/test_password/test_password.py
+++ tests/test_password/test_password.py
@@ -47,38 +47,3 @@ def test_encrypted_repo(rpc, enc_version):
     assert api.is_password_set(repo.id, USER) == 0
 
     api.remove_repo(repo_id)
-
-@pytest.mark.parametrize('algo',
-                         [('argon2id'), ('pbkdf2_sha256')])
-def test_encrypted_repo_algo(algo):
-    enc_version = 4
-    test_repo_name = 'test_enc_repo'
-    test_repo_desc = 'test_enc_repo'
-    test_repo_passwd = 'test_enc_repo'
-    repo_id = 'ba21bf89-f538-4196-9798-306fc70c3273'
-    enc_info = api.generate_magic_and_random_key(enc_version, repo_id, test_repo_passwd, algo)
-    assert enc_info
-    ret_repo_id = api.create_enc_repo(repo_id, test_repo_name, test_repo_desc,
-                                      USER, enc_info.magic, enc_info.random_key,
-                                      enc_info.salt, enc_version, enc_info.pwd_hash, algo)
-    assert ret_repo_id == repo_id
-
-    repo = api.get_repo(repo_id)
-    assert repo
-    assert repo.enc_version == enc_version
-    assert len(repo.pwd_hash) == 64
-    assert len(repo.random_key) == 96
-    assert len(repo.salt) == 64
-        
-    new_passwd = 'new password'
-
-    assert api.set_passwd(repo.id, USER, test_repo_passwd) == 0
-    assert api.get_decrypt_key(repo.id, USER)
-    api.change_repo_passwd(repo.repo_id, test_repo_passwd, new_passwd, USER) == 0
-    assert api.set_passwd(repo.id, USER, new_passwd) == 0
-
-    assert api.is_password_set(repo.id, USER)
-    assert api.unset_passwd(repo.id, USER) == 0
-    assert api.is_password_set(repo.id, USER) == 0
-
-    api.remove_repo(repo_id)
