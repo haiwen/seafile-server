445188fc0f5bdac4badc339116fe279fc97739d8
Heran Yang <heran.yang@seafile.com>
2025-08-04 03:18:18 +0000

    Fsck check if file is corrupt

--- server/gc/fsck.h
+++ server/gc/fsck.h
@@ -2,7 +2,7 @@
 #define SEAF_FSCK_H
 
 int
-seaf_fsck (GList *repo_id_list, gboolean repair, int max_thread_num);
+seaf_fsck (GList *repo_id_list, gboolean repair, int max_thread_num, gboolean check_corrupt);
 
 void export_file (GList *repo_id_list, const char *seafile_dir, char *export_path);
 
--- server/gc/seaf-fsck.c
+++ server/gc/seaf-fsck.c
@@ -14,7 +14,7 @@ static char *central_config_dir = NULL;
 
 SeafileSession *seaf;
 
-static const char *short_opts = "hvft:c:d:rE:F:";
+static const char *short_opts = "hvft:c:d:rE:F:C";
 static const struct option long_opts[] = {
     { "help", no_argument, NULL, 'h', },
     { "version", no_argument, NULL, 'v', },
@@ -25,6 +25,7 @@ static const struct option long_opts[] = {
     { "config-file", required_argument, NULL, 'c', },
     { "central-config-dir", required_argument, NULL, 'F' },
     { "seafdir", required_argument, NULL, 'd', },
+    { "check-corruption", no_argument, NULL, 'C' },
     { 0, 0, 0, 0, },
 };
 
@@ -91,6 +92,7 @@ main(int argc, char *argv[])
     int c;
     gboolean repair = FALSE;
     gboolean force = FALSE;
+    gboolean check_corrupt = FALSE;
     char *export_path = NULL;
     int max_thread_num = 0;
 
@@ -109,12 +111,12 @@ main(int argc, char *argv[])
         case 'v':
             exit(-1);
             break;
-	case 'f':
-	    force = TRUE;
-	    break;
-	case 't':
-	    max_thread_num = atoi(strdup(optarg));
-	    break;
+        case 'f':
+            force = TRUE;
+            break;
+        case 't':
+            max_thread_num = atoi(strdup(optarg));
+            break;
         case 'r':
             repair = TRUE;
             break;
@@ -129,6 +131,9 @@ main(int argc, char *argv[])
             break;
         case 'F':
             central_config_dir = strdup(optarg);
+            break;
+        case 'C':
+            check_corrupt = TRUE;
             break;
         default:
             usage();
@@ -173,7 +178,7 @@ main(int argc, char *argv[])
     if (export_path) {
         export_file (repo_id_list, seafile_dir, export_path);
     } else {
-        seaf_fsck (repo_id_list, repair, max_thread_num);
+        seaf_fsck (repo_id_list, repair, max_thread_num, check_corrupt);
     }
 
     return 0;
