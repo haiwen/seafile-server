e8745a242e82235dea5175fcc62f1f7b613277b5
Heran Yang <heran.yang@seafile.com>
2025-08-25 09:37:20 +0000

    Add LoadDBOption in option pkg

--- fileserver/fileserver.go
+++ fileserver/fileserver.go
@@ -29,7 +29,6 @@ import (
 	"github.com/haiwen/seafile-server/fileserver/share"
 	"github.com/haiwen/seafile-server/fileserver/utils"
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/ini.v1"
 
 	"net/http/pprof"
 )
@@ -90,7 +89,7 @@ func (f *LogFormatter) Format(entry *log.Entry) ([]byte, error) {
 }
 
 func loadCcnetDB() {
-	dbOpt, err := loadDBOption()
+	dbOpt, err := option.LoadDBOption(centralDir)
 	if err != nil {
 		log.Fatalf("Failed to load database: %v", err)
 	}
@@ -117,129 +116,6 @@ func loadCcnetDB() {
 	ccnetDB.SetMaxIdleConns(8)
 }
 
-func loadDBOption() (*option.DBOption, error) {
-	dbOpt, err := loadDBOptionFromFile()
-	if err != nil {
-		log.Warnf("failed to load database config: %v", err)
-	}
-	dbOpt = loadDBOptionFromEnv(dbOpt)
-
-	if dbOpt.Host == "" {
-		return nil, fmt.Errorf("no database host in seafile.conf.")
-	}
-	if dbOpt.User == "" {
-		return nil, fmt.Errorf("no database user in seafile.conf.")
-	}
-	if dbOpt.Password == "" {
-		return nil, fmt.Errorf("no database password in seafile.conf.")
-	}
-
-	return dbOpt, nil
-}
-
-func loadDBOptionFromEnv(dbOpt *option.DBOption) *option.DBOption {
-	user := os.Getenv("SEAFILE_MYSQL_DB_USER")
-	password := os.Getenv("SEAFILE_MYSQL_DB_PASSWORD")
-	host := os.Getenv("SEAFILE_MYSQL_DB_HOST")
-	ccnetDbName := os.Getenv("SEAFILE_MYSQL_DB_CCNET_DB_NAME")
-	seafileDbName := os.Getenv("SEAFILE_MYSQL_DB_SEAFILE_DB_NAME")
-
-	if dbOpt == nil {
-		dbOpt = new(option.DBOption)
-	}
-	if user != "" {
-		dbOpt.User = user
-	}
-	if password != "" {
-		dbOpt.Password = password
-	}
-	if host != "" {
-		dbOpt.Host = host
-	}
-	if dbOpt.Port == 0 {
-		dbOpt.Port = 3306
-	}
-	if ccnetDbName != "" {
-		dbOpt.CcnetDbName = ccnetDbName
-	} else if dbOpt.CcnetDbName == "" {
-		dbOpt.CcnetDbName = "ccnet_db"
-		log.Infof("Failed to read SEAFILE_MYSQL_DB_CCNET_DB_NAME, use ccnet_db by default")
-	}
-	if seafileDbName != "" {
-		dbOpt.SeafileDbName = seafileDbName
-	} else if dbOpt.SeafileDbName == "" {
-		dbOpt.SeafileDbName = "seafile_db"
-		log.Infof("Failed to read SEAFILE_MYSQL_DB_SEAFILE_DB_NAME, use seafile_db by default")
-	}
-	return dbOpt
-}
-
-func loadDBOptionFromFile() (*option.DBOption, error) {
-	dbOpt := new(option.DBOption)
-	dbOpt.DBEngine = "mysql"
-
-	seafileConfPath := filepath.Join(centralDir, "seafile.conf")
-	opts := ini.LoadOptions{}
-	opts.SpaceBeforeInlineComment = true
-	config, err := ini.LoadSources(opts, seafileConfPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to load seafile.conf: %v", err)
-	}
-
-	section, err := config.GetSection("database")
-	if err != nil {
-		return dbOpt, nil
-	}
-
-	dbEngine := "mysql"
-	key, err := section.GetKey("type")
-	if err == nil {
-		dbEngine = key.String()
-	}
-	if dbEngine != "mysql" {
-		return nil, fmt.Errorf("unsupported database %s.", dbEngine)
-	}
-	dbOpt.DBEngine = dbEngine
-	if key, err = section.GetKey("host"); err == nil {
-		dbOpt.Host = key.String()
-	}
-	// user is required.
-	if key, err = section.GetKey("user"); err == nil {
-		dbOpt.User = key.String()
-	}
-
-	if key, err = section.GetKey("password"); err == nil {
-		dbOpt.Password = key.String()
-	}
-
-	if key, err = section.GetKey("db_name"); err == nil {
-		dbOpt.SeafileDbName = key.String()
-	}
-	port := 3306
-	if key, err = section.GetKey("port"); err == nil {
-		port, _ = key.Int()
-	}
-	dbOpt.Port = port
-	useTLS := false
-	if key, err = section.GetKey("use_ssl"); err == nil {
-		useTLS, _ = key.Bool()
-	}
-	dbOpt.UseTLS = useTLS
-	skipVerify := false
-	if key, err = section.GetKey("skip_verify"); err == nil {
-		skipVerify, _ = key.Bool()
-	}
-	dbOpt.SkipVerify = skipVerify
-	if key, err = section.GetKey("ca_path"); err == nil {
-		dbOpt.CaPath = key.String()
-	}
-	if key, err = section.GetKey("connection_charset"); err == nil {
-		dbOpt.Charset = key.String()
-	}
-
-	return dbOpt, nil
-}
-
 // registerCA registers CA to verify server cert.
 func registerCA(capath string) {
 	rootCertPool := x509.NewCertPool()
@@ -256,7 +132,7 @@ func registerCA(capath string) {
 }
 
 func loadSeafileDB() {
-	dbOpt, err := loadDBOption()
+	dbOpt, err := option.LoadDBOption(centralDir)
 	if err != nil {
 		log.Fatalf("Failed to load database: %v", err)
 	}
@@ -282,7 +158,6 @@ func loadSeafileDB() {
 	seafileDB.SetConnMaxLifetime(5 * time.Minute)
 	seafileDB.SetMaxOpenConns(8)
 	seafileDB.SetMaxIdleConns(8)
-	option.DBType = dbOpt.DBEngine
 }
 
 func writePidFile(pid_file_path string) error {
