a8c1a352d761fa257a7ae05b00394125d5301af4
Heran Yang <heran.yang@seafile.com>
2026-03-11 02:49:17 +0000

    Support SEAFILE_MYSQL_DB_PORT env

--- fileserver/option/option.go
+++ fileserver/option/option.go
@@ -432,6 +432,7 @@ func loadDBOptionFromEnv(dbOpt *DBOption) *DBOption {
 	user := os.Getenv("SEAFILE_MYSQL_DB_USER")
 	password := os.Getenv("SEAFILE_MYSQL_DB_PASSWORD")
 	host := os.Getenv("SEAFILE_MYSQL_DB_HOST")
+	portStr := os.Getenv("SEAFILE_MYSQL_DB_PORT")
 	ccnetDbName := os.Getenv("SEAFILE_MYSQL_DB_CCNET_DB_NAME")
 	seafileDbName := os.Getenv("SEAFILE_MYSQL_DB_SEAFILE_DB_NAME")
 
@@ -446,6 +447,12 @@ func loadDBOptionFromEnv(dbOpt *DBOption) *DBOption {
 	}
 	if host != "" {
 		dbOpt.Host = host
+	}
+	if portStr != "" {
+		port, _ := strconv.ParseUint(portStr, 10, 32)
+		if port > 0 {
+			dbOpt.Port = int(port)
+		}
 	}
 	if dbOpt.Port == 0 {
 		dbOpt.Port = 3306
